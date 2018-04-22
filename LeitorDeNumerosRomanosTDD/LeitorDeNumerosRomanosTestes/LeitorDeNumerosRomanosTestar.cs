405946e21d76fb1335c848d196ec442423fbc413
Renan Rolo <renanrolo@hotmail.com>
2018-04-22 13:46:05 -0400

    projeto

---
+++ LeitorDeNumerosRomanosTDD/LeitorDeNumerosRomanosTestes/LeitorDeNumerosRomanosTestar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeitorDeNumerosRomanos;
+
+namespace LeitorDeNumerosRomanosTestes
+{
+    [TestClass]
+    public class LeitorDeNumerosRomanosTestar
+    {
+        [TestMethod]
+        public void TestarInstancia()
+        {
+            NumerosRomanos leitorDeNumerosRomanos = new NumerosRomanos();
+
+            Assert.IsNotNull(leitorDeNumerosRomanos);
+        }
+    }
+}
